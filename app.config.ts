7d1f7df64b240536d2bbeaf5be91f534a6469a8e
qiokio <59764006+qiokio@users.noreply.github.com>
2025-04-24 20:55:12 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
   title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
+  email: '/',
+  github: 'https://github.com/qiokio/sink',
   twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  telegram: '/',
+  mastodon: '/',
+  blog: 'https://prooo.me',
+  description: 'A Simple / Speedy / Secure Link Shortener with Analytics.',
+  image: '/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
